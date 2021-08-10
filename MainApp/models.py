00bbbbbcddcff53144fa4dd0355c38cf8494c9fb
boo <boo>
2021-08-10 10:21:29 -0700

    fix form Auth

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,5 +1,6 @@
 import datetime
 from django.db import models
+from django.contrib.auth.models import User
 
 LANG_CHOICES = (
     ("py", "python"),
@@ -8,7 +9,9 @@ LANG_CHOICES = (
 )
 
 class Snippet(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=True)
     lang = models.CharField(max_length=30, choices=LANG_CHOICES)
     code = models.TextField(max_length=5000)
     creation_date = models.DateTimeField(default=datetime.datetime.now)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE,
+                             blank=True, null=True)
